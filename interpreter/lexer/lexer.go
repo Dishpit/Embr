bbed856264c825cf715cb2fd6af27dfabbc826a8
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-10 13:27:15 -0500

    set up readChar() function

--- interpreter/lexer/lexer.go
+++ interpreter/lexer/lexer.go
@@ -11,3 +11,18 @@ func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	return l
 }
+
+/* gives the next character and advance our position in the input string.
+check whether we reach the end of input, if so set l.ch to 0, otherwise set l.ch to the next character then increment by one.
+
+with the current implementation, Omega only supports ASCII instead of full unicode.
+in order to fix this, l.ch needs to be a rune instead of a byte since characters could be multiple bytes wide, and then other functions would need to change to accomodate this.*/
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	}	else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}

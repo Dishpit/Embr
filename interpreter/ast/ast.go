08c6402abe656ee06ae8cfc363f6623d96b33fbc
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-11 16:44:15 -0500

    parse return statements

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -2,6 +2,13 @@ package ast
 
 import "interpreter/token"
 
+type ReturnStatement struct {
+	Token token.Token
+	ReturnValue Expression
+}
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
 // variable types
 type VarTypeInt struct {
 	Token token.Token	// the token.VARTYPE_INT token

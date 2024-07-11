e2f9db1c98dcf82f9e7e3b8e63976b6826cfb91d
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-11 16:13:32 -0500

    set up initial parser test for int type statement

---
+++ interpreter/parser/parser_test.go
@@ -0,0 +1,64 @@
+package parser
+
+import (
+	"testing"
+	"interpreter/ast"
+	"interpreter/lexer"
+)
+
+func TestVarTypeIntStatements(t *testing.T) {
+	input := `
+int x = 420;
+int y = 69;
+int foobar = 8675309
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		f.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testVarTypeIntStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testVarTypeIntStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "int" {
+		t.Errorf("s.TokenLiteral not 'int'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	intStmt, ok := s.(*ast.IntStatement)
+	if !ok {
+		t.Errorf("s not *ast.IntStatement. got=%T", s)
+		return false
+	}
+
+	if intStmt.Name.Value != name {
+		t.Errorf("intStmt.Name.Value not '%s'. got=%s", name, intStmt.Name.Value)
+		return false
+	}
+
+	if intStmt.Name.TokenLiteral() != name {
+		t.Errorf("intStmt.Name.TokenLiteral() not '%s'. got=%s", name, intStmt.Name.TokenLiteral())
+		return false
+	}
+	return true
+}

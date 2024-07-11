2dfd62dd4a9a007f25dfc93a7e068eed9ef25693
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-11 15:49:50 -0500

    set up program node to use as root of AST

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -13,3 +13,15 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}	else {
+		return ""
+	}
+}

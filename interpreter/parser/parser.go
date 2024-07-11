d2afddf956f0603fa04f051a15218b5c8f59ecbc
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-11 16:17:09 -0500

    flesh out parseprogram()

--- interpreter/parser/parser.go
+++ interpreter/parser/parser.go
@@ -28,5 +28,15 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
 }

65b7885363e5e3b0a5af0417c06e5f5625f024ca
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-12 15:55:58 -0500

    register parseIntegerLiteral method in new

--- interpreter/parser/parser.go
+++ interpreter/parser/parser.go
@@ -42,6 +42,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// read two tokens so curtoken and peektoken are both set
 	p.nextToken()

37e566f584f383fa4e5915bc0452cbec74183414
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-14 11:35:04 -0500

    define parse infix expression function

--- interpreter/parser/parser.go
+++ interpreter/parser/parser.go
@@ -100,6 +100,20 @@ func (p *Parser) parsePrefixExpression() ast.Expression {
 	return expression
 }
 
+func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+	expression := &ast.InfixExpression{
+		Token: p.curToken,
+		Operator: p.curToken.Literal,
+		Left: left,
+	}
+
+	precedence := p.curPrecedence()
+	p.nextToken()
+	expression.Right = p.parseExpression(precedence)
+
+	return expression
+}
+
 func (p *Parser) parseIntegerLiteral() ast.Expression {
 	lit := &ast.IntegerLiteral{Token: p.curToken}
 

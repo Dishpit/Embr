cb13b5c8a7f9abf73e24d44fa5443812f0303b00
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-14 11:33:19 -0500

    register infix operators

--- interpreter/parser/parser.go
+++ interpreter/parser/parser.go
@@ -57,6 +57,16 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
+	p.infixParseFns = make(map[token.TokenType]infixParseFn)
+	p.registerInfix(token.PLUS, p.parseInfixExpression)
+	p.registerInfix(token.MINUS, p.parseInfixExpression)
+	p.registerInfix(token.SLASH, p.parseInfixExpression)
+	p.registerInfix(token.ASTERISK, p.parseInfixExpression)
+	p.registerInfix(token.EQ, p.parseInfixExpression)
+	p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
+	p.registerInfix(token.LT, p.parseInfixExpression)
+	p.registerInfix(token.GT, p.parseInfixExpression)
+
 	// read two tokens so curtoken and peektoken are both set
 	p.nextToken()
 	p.nextToken()

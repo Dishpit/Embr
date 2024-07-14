e3d69e59948355cd3ecfd8081970e8f53af45883
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-14 11:26:59 -0500

    define ast.InfixExpression

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -5,6 +5,26 @@ import (
 	"bytes"
 )
 
+type InfixExpression struct {
+	Token token.Token
+	Left Expression
+	Operator string
+	Right Expression
+}
+func (ie *InfixExpression) expressionNode() {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
 type PrefixExpression struct {
 	Token token.Token
 	Operator string

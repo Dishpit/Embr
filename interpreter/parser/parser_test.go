c89092579c5cdc4508911cc2fdf2a50999ec1dc7
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-15 11:38:43 -0500

    prepare testidentifier func

--- interpreter/parser/parser_test.go
+++ interpreter/parser/parser_test.go
@@ -7,6 +7,26 @@ import (
 	"fmt"
 )
 
+func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+	ident, ok := exp.(*ast.Identifier)
+	if !ok {
+		t.Errorf("exp not *ast.Identifier. got=%T", exp)
+		return false
+	}
+
+	if ident.Value != value {
+		t.Errorf("ident.Value not %s. got=%s", value, ident.Value)
+		return false
+	}
+
+	if ident.TokenLiteral() != value {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", value, ident.TokenLiteral())
+		return false
+	}
+
+	return true
+}
+
 func TestParsingInfixExpressions(t *testing.T) {
 	infixTests := []struct {
 		input string

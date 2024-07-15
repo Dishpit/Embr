397f81ad6be237e11de35f2c6891fd94f47cdb92
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-15 11:48:17 -0500

    set up boolean struct

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -5,6 +5,14 @@ import (
 	"bytes"
 )
 
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+func (b *Boolean) expressionNode() {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string { return b.Token.Literal }
+
 type InfixExpression struct {
 	Token token.Token
 	Left Expression

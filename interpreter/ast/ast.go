2d1ece71f40f7c2d18494aa369eeee4924405b59
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-22 11:16:54 -0500

    define if expression AST node

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -5,6 +5,30 @@ import (
 	"bytes"
 )
 
+type IfExpression struct {
+	Token token.Token
+	Condition Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode() {}
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else")
+		out.WriteString(ie.Alternative.String())
+	}
+	return out.String()
+}
+
 type Boolean struct {
 	Token token.Token
 	Value bool

4f9a71547f94942a612ea08ab5c09b431982b9a0
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-22 11:29:55 -0500

    define parseBlockStatement func

--- interpreter/parser/parser.go
+++ interpreter/parser/parser.go
@@ -103,6 +103,22 @@ func (p *Parser) parseIfExpression() ast.Expression {
 	return expression
 }
 
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+
+	p.nextToken()
+
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return block
+}
+
 func (p *Parser) parseGroupedExpression() ast.Expression {
 	p.nextToken()
 

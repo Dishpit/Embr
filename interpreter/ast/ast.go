5b1da10738c60e6e5db7a7e601876dcd616fd66b
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-24 17:59:38 -0500

    set up callexpression ast node

--- interpreter/ast/ast.go
+++ interpreter/ast/ast.go
@@ -6,6 +6,29 @@ import (
 	"strings"
 )
 
+type CallExpression struct {
+	Token token.Token
+	Function Expression
+	Arguments []Expression
+}
+func (ce *CellExpression) expressionNode() {}
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
+
 type FunctionLiteral struct {
 	Token token.Token
 	Parameters []*Identifier

6d9e49c32e01dc37a73a3dfed40abb6cdf05b0b4
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-25 02:15:18 -0500

    set up evaluator tests

---
+++ interpreter/evaluator/evaluator_test.go
@@ -0,0 +1,45 @@
+package evaluator
+
+import (
+	"interpreter/lexer"
+	"interpreter/object"
+	"interpreter/parser"
+	"tesing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	test := []struct {
+		input string
+		expected int64
+	}{
+		{"420", 420},
+		{"69", 69},
+	}
+
+	for _, tt := range tests {
+		evaluated = testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parse.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Intger. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d",
+	result.Value, expected)
+	return false
+	}
+	return true
+}

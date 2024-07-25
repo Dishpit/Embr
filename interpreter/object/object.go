7b8275ebe9dd5a6b9e4a8abd3665e66c59f3a170
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-25 02:02:41 -0500

    set up boolean object

--- interpreter/object/object.go
+++ interpreter/object/object.go
@@ -4,10 +4,10 @@ import "fmt"
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
 )
 
 type ObjectType string
-
 type Object interface {
 	Type() ObjectType
 	Inspect() string
@@ -18,3 +18,9 @@ type Integer struct {
 }
 func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+type Boolean struct {
+	Value bool
+}
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }

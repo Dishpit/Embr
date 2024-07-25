0987255436b01211944958efd3c9b64557a6d01f
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-25 05:44:59 -0500

    test func object

--- interpreter/evaluator/evaluator_test.go
+++ interpreter/evaluator/evaluator_test.go
@@ -7,6 +7,30 @@ import (
 	"testing"
 )
 
+func TestFunctionObject(t *testing.T) {
+	input := "fn my_func(x) @void {x + 2;};"
+
+	evaluated := testEval(input)
+	fn, ok := evaluated.(*object.Function)
+	if !ok {
+		t.Fatalf("object is not Function. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if len(fn.Parameters) != 1 {
+		t.Fatalf("function has wrong parameters. Parameters=%+v", fn.Parameters)
+	}
+
+	if fn.Parameters[0].String() != "x" {
+		t.Fatalf("parameter is not 'x'. got=%q", fn.Parameters[0])
+	}
+
+	expectedBody := "(x + 2)"
+
+	if fn.Body.String() != expectedBody {
+		t.Fatalf("body is not %q. got=%q", expectedBody, fn.Body.String())
+	}
+}
+
 func TestIntStatements(t *testing.T) {
 	tests := []struct {
 		input string

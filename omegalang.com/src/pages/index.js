6aa4f17b5df4c9f2d7a859a80a800dafdd58d16b
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-25 14:34:13 -0500

    update versioning

--- omegalang.com/src/pages/index.js
+++ omegalang.com/src/pages/index.js
@@ -23,7 +23,7 @@ function HomepageHeader() {
             Check Out Omega! 💻
           </Link>
         </div>
-        <p>Current version: v0.6.1</p>
+        <p>Current version: v0.6.2</p>
       </div>
     </header>
   );

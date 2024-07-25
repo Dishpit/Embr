c95da29e504c3e62f200a23b64862b98dffee298
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-25 04:26:22 -0500

    return results

--- interpreter/evaluator/evaluator.go
+++ interpreter/evaluator/evaluator.go
@@ -51,11 +51,13 @@ func evalBlockStatement(block *ast.BlockStatement) object.Object {
 	for _, statement := range block.Statements {
 		result = Eval(statement)
 
-		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
-			return result
+		if result != nil {
+			rt := result.Type()
+			if rt == object.RETURN_VALUE_OBJ || rt == object.ERROR_OBJ {
+				return result
+			}
 		}
 	}
-
 	return result
 }
 
@@ -176,10 +178,12 @@ func evalProgram(program *ast.Program) object.Object {
 	for _, statement := range program.Statements {
 		result = Eval(statement)
 
-		if returnValue, ok := result.(*object.ReturnValue); ok {
-			return returnValue.Value
+		switch result := result.(type) {
+		case *object.ReturnValue:
+			return result.Value
+		case *object.Error:
+			return result
 		}
 	}
-
 	return result
 }

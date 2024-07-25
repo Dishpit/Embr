b928118a58d78726bd34ec714aa48e12c926bdc1
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-25 01:33:15 -0500

    set up integer object type

--- interpreter/object/object.go
+++ interpreter/object/object.go
@@ -1,8 +1,20 @@
 package object
 
+import "fmt"
+
+const (
+	INTEGER_OBJ = "INTEGER"
+)
+
 type ObjectType string
 
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+type Integer struct {
+	Value int64
+}
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }

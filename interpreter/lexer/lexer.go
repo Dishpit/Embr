e13ed7dae150fcebde33916c03ed25f36754a2e8
Zebulun McNeill <zeb.mcn@gmail.com>
2024-07-31 04:40:49 -0500

    lexing hash literals

--- interpreter/lexer/lexer.go
+++ interpreter/lexer/lexer.go
@@ -82,6 +82,8 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACKET, l.ch)
 	case ']':
 		tok = newToken(token.RBRACKET, l.ch)
+	case ':':
+		tok = newToken(token.COLON, l.ch)
 	case '@':
 		tok = newToken(token.FN_RETURN, l.ch)
 		l.readChar()

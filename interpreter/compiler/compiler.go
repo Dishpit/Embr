7b88adf192a0af9df52f4a2d826e45939bb2ecbb
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-01 17:20:30 -0500

    use symbols in the compiler

--- interpreter/compiler/compiler.go
+++ interpreter/compiler/compiler.go
@@ -12,6 +12,7 @@ type Compiler struct {
 	constants []object.Object
 	lastInstruction EmittedInstruction
 	previousInstruction EmittedInstruction
+	symbolTable *SymbolTable
 }
 
 type EmittedInstruction struct {
@@ -25,6 +26,7 @@ func New() *Compiler {
 		constants: []object.Object{},
 		lastInstruction: EmittedInstruction{},
 		previousInstruction: EmittedInstruction{},
+		symbolTable: NewSymbolTable(),
 	}
 }
 
@@ -160,11 +162,20 @@ func (c *Compiler) Compile(node ast.Node) error {
 			}
 		}
 
+	case *ast.Identifier:
+		symbol, ok := c.symbolTable.Resolve(node.Value)
+		if !ok {
+			return fmt.Errorf("undefined variable %s", node.Value)
+		}
+		c.emit(code.OpGetGlobal, symbol.Index)
+
 	case *ast.TypeInt:
 		err := c.Compile(node.Value)
 		if err != nil {
 			return err
 		}
+		symbol := c.symbolTable.Define(node.Name.Value)
+		c.emit(code.OpSetGlobal, symbol.Index)
 	}
 
 	return nil

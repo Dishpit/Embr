e145fdcd56249e2b499921a47db5f3760d944913
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-01 08:12:24 -0500

    executing jumps

--- interpreter/vm/vm.go
+++ interpreter/vm/vm.go
@@ -82,9 +82,27 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpJump:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip = pos - 1
+		case code.OpJumpNotTruthy:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+			condition := vm.pop()
+			if !isTruthy(condition) {
+				ip = pos - 1
+			}
 		}
 	}
 	return nil
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj := obj.(type) {
+	case *object.Boolean:
+		return obj.Value
+	default: return true
+	}
 }
 
 func (vm *VM) executeMinusOperator() error {

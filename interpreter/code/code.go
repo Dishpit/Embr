403cf63ec6cc1a6891854b161dbb51e13dc8c8a7
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-01 05:06:09 -0500

    first opcode

--- interpreter/code/code.go
+++ interpreter/code/code.go
@@ -1,6 +1,9 @@
 package code
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+)
 
 const (
 	OpConstant Opcode = iota
@@ -26,4 +29,31 @@ func Lookup(op byte) (*Definition, error) {
 	}
 
 	return def, nil
+}
+
+func Make(op Opcode, operands ...int) []byte {
+	def, ok := definitions[op]
+	if !ok {
+		return []byte{}
+	}
+
+	instructionLen := 1
+	for _, w := range def.OperandWidths {
+		instructionLen += w
+	}
+
+	instruction := make([]byte, instructionLen)
+	instruction[0] = byte(op)
+
+	offset := 1
+	for i, o := range operands {
+		width := def.OperandWidths[i]
+		switch width {
+		case 2:
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		}
+		offset += width
+	}
+
+	return instruction
 }

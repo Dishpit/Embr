0cb43584c48245aee3105aa88e08af2f118cb893
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-01 06:18:25 -0500

    initial virtual machine

--- interpreter/repl/repl.go
+++ interpreter/repl/repl.go
@@ -4,17 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"interpreter/compiler"
+	"interpreter/vm"
 	"interpreter/lexer"
 	"interpreter/parser"
-	"interpreter/evaluator"
-	"interpreter/object"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -33,11 +32,23 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "SKILL ISSUE! Compilation failed:\n %s\n", err)
+			continue
 		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "SKILL ISSUE! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
 

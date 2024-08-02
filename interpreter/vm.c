16d3f2f4117fffc59e1cc7f538ecf75a29413581
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-02 10:08:44 -0500

    binary operators

--- interpreter/vm.c
+++ interpreter/vm.c
@@ -31,6 +31,12 @@ Value pop() {
 static InterpretResult run() {
   #define READ_BYTE() (*vm.ip++)
   #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+  #define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)
 
   for (;;) {
     #ifdef DEBUG_TRACE_EXECUTION
@@ -50,7 +56,11 @@ static InterpretResult run() {
         push(constant);
         break;
       }
-      case OP_NEGATE: push(-pop()); break;
+      case OP_ADD:      BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
+      case OP_NEGATE:   push(-pop()); break;
       case OP_RETURN: {
         printValue(pop());
         printf("\n");
@@ -61,6 +71,7 @@ static InterpretResult run() {
 
   #undef READ_BYTE
   #undef READ_CONSTANT
+  #undef BINARY_OP
 }
 
 

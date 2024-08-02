2eecbc7c6bdf68a97b08511f5015d1c2bb47588f
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-02 18:20:41 -0500

    conditionals and control flow

--- interpreter/debug.c
+++ interpreter/debug.c
@@ -71,6 +71,12 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       return simpleInstruction("OP_NEGATE", offset);
     case OP_OUT:
       return simpleInstruction("OP_OUT", offset);
+    case OP_JUMP:
+      return jumpInstruction("OP_JUMP", 1, chunk, offset);
+    case OP_JUMP_IF_FALSE:
+      return jumpInstruction("OP_JUMP_IF_FALSE", 1, chunk, offset);
+    case OP_LOOP:
+      return jumpInstruction("OP_LOOP", -1, chunk, offset);
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", offset);
     default:
@@ -89,4 +95,13 @@ static int byteInstruction(const char* name, Chunk* chunk,
   uint8_t slot = chunk->code[offset + 1];
   printf("%-16s %4d\n", name, slot);
   return offset + 2;
+}
+
+static int jumpInstruction(const char* name, int sign,
+                            Chunk* chunk, int offset) {
+  uint16_t jump = (uint16_t)(chunk->code[offset + 1] << 8);
+  jump |= chunk->code[offset + 2];
+  printf("%-16s %4d -> %d\n", name, offset,
+          offset + 3 + sign * jump);
+  return offset + 3;
 }

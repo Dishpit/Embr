1d694e5d89a740a7e37c7ec8c4986f201927d9a6
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-01 22:44:16 -0500

    disassembling line information

--- interpreter/main.c
+++ interpreter/main.c
@@ -6,9 +6,9 @@ int main(int argc, const char* argv[]) {
   Chunk chunk;
   initChunk(&chunk);
   int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT);
-  writeChunk(&chunk, constant);
-  writeChunk(&chunk, OP_RETURN);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+  writeChunk(&chunk, OP_RETURN, 123);
   disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
   return 0;

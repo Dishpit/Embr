2c1554128a9b4778fd7103bd061b9e82b86fbfff
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-01 22:11:48 -0500

    disassemble chunk

--- interpreter/debug.c
+++ interpreter/debug.c
@@ -9,3 +9,21 @@ void disassembleChunk(Chunk* chunk, const char* name) {
     offset = disassembleInstruction(chunk, offset);
   }
 }
+
+int disassembleInstruction(Chunk* chunk, int offset) {
+  printf("%04d ", offset);
+
+  uint8_t instruction = chunk->code[offset];
+  switch (instruction) {
+    case OP_RETURN:
+      return simpleInstruction("OP_RETURN", offset);
+    default:
+      printf("Unknown opcode %d\n", instruction);
+      return offset + 1;
+  }
+}
+
+static int simpleInstruction(const char* name, int offset) {
+  printf("%s\n", name);
+  return offset + 1;
+}

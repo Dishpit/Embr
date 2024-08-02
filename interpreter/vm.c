caf0c643555ca41d8a83cd9b08db51ed453a1a89
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-02 09:38:00 -0500

    vm stack

--- interpreter/vm.c
+++ interpreter/vm.c
@@ -6,12 +6,26 @@
 
 VM vm;
 
-void initVM() {
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
 
+void initVM() {
+  resetStack();
 }
 
 void freeVM() {
   
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
 }
 
 static InterpretResult run() {

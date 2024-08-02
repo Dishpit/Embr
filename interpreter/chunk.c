506c79650dc1a34a2bf8c34d9bdd7d6d9c56f483
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-01 21:42:55 -0500

    grow and free array chunks

--- interpreter/chunk.c
+++ interpreter/chunk.c
@@ -1,12 +1,17 @@
 #include <stdlib.h>
 
 #include "chunk.h"
-#include "memory."
+#include "memory.h"
 
 void initChunk(Chunk* chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+}
+
+void freeChunk(Chunk* chunk) {
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  initChunk(chunk);
 }
 
 void writeChunk(Chunk* chunk, uint8_t byte) {

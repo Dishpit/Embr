e0b903aac59bf62ac4d6d35d15cbafd2fd8653d5
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-01 22:29:07 -0500

    value arrays

--- interpreter/value.c
+++ interpreter/value.c
@@ -8,3 +8,19 @@ void initValueArray(ValueArray* array) {
   array->capacity = 0;
   array->count = 0;
 }
+
+void writeValueArray(ValueArray* array, Value value) {
+  if (array->capacity < array->count + 1) {
+    int oldCapacity = array->capacity;
+    array->capacity = GROW_CAPACITY(oldCapacity);
+    array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
+  }
+
+  array->values[array->count] = value;
+  array->count++;
+}
+
+void freeValueArray(ValueArray* array) {
+  FREE_ARRAY(Value, array->values, array->capacity);
+  initValueArray(array);
+}

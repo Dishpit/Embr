d6e3ab54d803b6d18d110933901e86c22c28a45e
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-02 17:43:39 -0500

    local variables

--- interpreter/debug.c
+++ interpreter/debug.c
@@ -40,6 +40,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       return simpleInstruction("OP_FALSE", offset);
     case OP_POP:
       return simpleInstruction("OP_POP", offset);
+    case OP_GET_LOCAL:
+      return byteInstruction("OP_GET_LOCAL", chunk, offset);
+    case OP_SET_LOCAL:
+      return byteInstruction("OP_SET_LOCAL", chunk, offset);
     case OP_GET_GLOBAL:
       return constantInstruction("OP_GET_GLOBAL", chunk, offset);
     case OP_DEFINE_GLOBAL:
@@ -78,4 +82,11 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 static int simpleInstruction(const char* name, int offset) {
   printf("%s\n", name);
   return offset + 1;
+}
+
+static int byteInstruction(const char* name, Chunk* chunk,
+                            int offset) {
+  uint8_t slot = chunk->code[offset + 1];
+  printf("%-16s %4d\n", name, slot);
+  return offset + 2;
 }

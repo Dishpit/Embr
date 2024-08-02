4fb9ccadc3ffb867b9d0a735af594485a9c5a2cf
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-01 22:02:33 -0500

    set up debug

---
+++ interpreter/debug.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "debug.h"
+
+void disassembleChunk(Chunk* chunk, const char* name) {
+  printf("== %s ==\n", name);
+
+  for (int offset = 0; offset < chunk->count;) {
+    offset = disassembleInstruction(chunk, offset);
+  }
+}

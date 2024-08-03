32483be247ce713d866488d26510e451e658bc78
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-03 04:15:19 -0500

    omegabyte 2024 page

---
+++ omegalang.com/src/pages/omegabyte.js
@@ -0,0 +1,34 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import OmegaByte2024Info from '@site/src/components/OmegaByte2024';
+
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+import OmegaByte2024 from '../../static/img/omegabyte-2024-02.png'
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <img src={OmegaByte2024} />
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title} | The General Purpose Programming`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <OmegaByte2024Info />
+      </main>
+    </Layout>
+  );
+}

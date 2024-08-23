501cbb95d9afe9256c50c4b68373716a58ebf034
Zebulun McNeill <zeb.mcn@gmail.com>
2024-08-23 16:53:32 -0500

    i suppose a name change warrants a larger semver

--- embrlang.com/src/pages/index.js
+++ embrlang.com/src/pages/index.js
@@ -23,7 +23,7 @@ function HomepageHeader() {
             Check Out Embr! 💻
           </Link>
         </div>
-        <p>Current version: v0.17.1</p>
+        <p>Current version: v0.18.0</p>
       </div>
     </header>
   );
